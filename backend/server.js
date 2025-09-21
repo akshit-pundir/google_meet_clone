5173ae8d7c85788736045dc8f1f612f1f7f613ab
akshit-pundir <akshit.pundir90@gmail.com>
2025-09-21 19:09:38 +0530

    remove https for production

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-const https = require("https");
 const fs = require("fs");
+const https = require("https");
+const http = require("http");
 const express = require("express");
 const socketIo = require("socket.io");
 const cors = require('cors')
@@ -11,14 +12,23 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 
 
-const key = fs.readFileSync("./certs/cert.key");
-const cert = fs.readFileSync("./certs/cert.crt");
+// const key = fs.readFileSync("./certs/cert.key");
+// const cert = fs.readFileSync("./certs/cert.crt"); //for https locally
 
 
-const expressServer = https.createServer({key, cert},app);
+// const expressServer = https.createServer({key, cert},app); for https locally
+const expressServer = http.createServer(app); 
+
+// const io = socketIo(expressServer,{
+//     cors:['https://localhost:5173']
+// });
+
 
 const io = socketIo(expressServer,{
-    cors:['https://localhost:5173']
+    cors:[
+            'http://www.hangout.linkpc.net/',
+            'http://api.hangout.linkpc.net/',
+         ]
 });
 
 expressServer.listen(8181);
